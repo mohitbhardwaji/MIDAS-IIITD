c0593205c67a3a6fdee2e22bc1cf451a628cc7c8
mohitbhardwaji <82044623+mohitbhardwaji@users.noreply.github.com>
2021-04-10 20:18:20 +0530

    Update loginf.php

--- loginf.php
+++ loginf.php
@@ -15,7 +15,74 @@
                <label>password</label>
                <input type="text" name="password" required>
        
+            </div><html>
+    <head>
+        <title>login page</title>
+        <link rel="stylesheet" href="loginstyling.css">
+    </head>
+ <body>
+     <div class="background">
+  <div class="form-cont">
+    <div class="container" id="contain" >
+            <h1>login</h1>
+           
+        <form action="http://mohitbhardwaj.site/login.php" method="post">
+       <div>
+         
+           <input type="text" name="user" placeholder="   username" required>
+        </div>
+        <div>
+            
+               <input type="text" name="password" placeholder="   password" required>
+       
             </div>
+            <button type="submit" value="submit">login</button>
+            
+       
+    </form>
+ 
+    <button onclick="toggle(this)" style="margin-left:15px;" >create account </button>
+   </div>
+  
+    <div class="reg" id="register">
+        <h1>regitration</h1>
+    <form action="http://mohitbhardwaj.site/registration.php" method="post">
+       <div>
+           
+           <input type="text" name="user" placeholder="   username" required>
+        </div>
+            <div>
+            
+                <input type="text" name="password" placeholder="   password" required>
+        
+            </div>
+            <button type="submit" value='submit'>signup</button>
+    </form>
+   
+    <a href="http://mohitbhardwaj.site/loginf.php">Back to login</a>
+    </div>
+ </div>
+</div>
+<script>
+  function toggle(y) {
+          var x = document.getElementById("contain");
+          var z= document.getElementById("register");
+          if (x.style.display == "block") {
+            x.style.display = "none";
+          } else {
+            x.style.display = "none";
+          }
+        
+        if (z.style.display == "block") {
+            z.style.display = "none";
+          } else {
+            z.style.display = "block";
+          }
+          
+        }
+</script>
+</body>
+</html>
             <button type="submit" value="submit">login</button>
        </div>
     </form>
