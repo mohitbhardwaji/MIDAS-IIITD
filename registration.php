272a48277265e1a7feaf08141dcea6ca3d9fac23
mohitbhardwaji <82044623+mohitbhardwaji@users.noreply.github.com>
2021-04-10 21:00:28 +0530

    Update registration.php

--- registration.php
+++ registration.php
@@ -1,25 +1,12 @@
 <?php
 
 session_start();
-header('Location:loginf.html');
-$server = "localhost";
-$pass = "";
-$name = "root";
-
-$con = new mysqli($server,$name,$pass);
-// if ($con->connect_error) {
-//     die("Connection failed: " . $con->connect_error);
-//   }
-//  else{ echo "Connected successfully";
-//  }<?php
-
-session_start();
-
+// db login details
 
 $server = "localhost";
 $pass = "Gold@12345";
 $name = "sumbha40_useraccounts";
-
+// making connection to the server
 $con = new mysqli($server,$name,$pass);
  if ($con->connect_error) {
      die("Connection failed: " . $con->connect_error);
@@ -29,7 +16,7 @@ $con = new mysqli($server,$name,$pass);
 mysqli_select_db($con,'sumbha40_useraccounts');
 $username            = $_POST['user'];
 $password        = $_POST['password'];
-
+// checking username if already exist or not
 $s ="SELECT * FROM user1 WHERE  username ='$username'";
 
 $result = mysqli_query($con,$s);
@@ -40,34 +27,12 @@ if($num == 1){
     echo"email already taken";
 }
 else{
-    
+//     adding user details to the db
     $reg = "INSERT INTO user1(username,password) VALUES ('$username','$password')";
     mysqli_query($con,$reg);
     echo"registration successfull";
     header('Location:http://mohitbhardwaj.site/loginf.php');
 }
-}
-
-?>
-mysqli_select_db($con,'useraccounts');
-$username            = $_POST['user'];
-// $email         = $_POST['user-email'];
-$password        = $_POST['password'];
-
-$s ="SELECT * FROM usertable WHERE  username ='$username'";
-
-$result = mysqli_query($con,$s);
-
-$num = mysqli_num_rows($result);
-
-if($num == 1){
-    echo"email already taken";
-}
-else{
-    $reg = "INSERT INTO usertable(username,password) VALUES ('$username','$password')";
-    mysqli_query($con,$reg);
-    echo"registration successfull";
-
 }
 
 ?>
