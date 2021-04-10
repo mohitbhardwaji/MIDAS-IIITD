8b0d541e9826d985e0f8c91140dac83fe29c8eac
mohitbhardwaji <82044623+mohitbhardwaji@users.noreply.github.com>
2021-04-10 20:22:22 +0530

    Update registration.php

--- registration.php
+++ registration.php
@@ -11,7 +11,44 @@ $con = new mysqli($server,$name,$pass);
 //     die("Connection failed: " . $con->connect_error);
 //   }
 //  else{ echo "Connected successfully";
-//  }
+//  }<?php
+
+session_start();
+
+
+$server = "localhost";
+$pass = "Gold@12345";
+$name = "sumbha40_useraccounts";
+
+$con = new mysqli($server,$name,$pass);
+ if ($con->connect_error) {
+     die("Connection failed: " . $con->connect_error);
+   }
+  else{ 
+ 
+mysqli_select_db($con,'sumbha40_useraccounts');
+$username            = $_POST['user'];
+$password        = $_POST['password'];
+
+$s ="SELECT * FROM user1 WHERE  username ='$username'";
+
+$result = mysqli_query($con,$s);
+
+$num = mysqli_num_rows($result);
+
+if($num == 1){
+    echo"email already taken";
+}
+else{
+    
+    $reg = "INSERT INTO user1(username,password) VALUES ('$username','$password')";
+    mysqli_query($con,$reg);
+    echo"registration successfull";
+    header('Location:http://mohitbhardwaj.site/loginf.php');
+}
+}
+
+?>
 mysqli_select_db($con,'useraccounts');
 $username            = $_POST['user'];
 // $email         = $_POST['user-email'];
