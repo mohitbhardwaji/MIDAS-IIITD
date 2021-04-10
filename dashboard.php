35d137a459acab6c0e30a994ffb82677a49bad6a
mohitbhardwaji <82044623+mohitbhardwaji@users.noreply.github.com>
2021-04-10 20:39:19 +0530

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -10,6 +10,7 @@ session_start();
     </head>
     <body>
         <div>
+            <!--normal loged in page to check every thing is good--> 
             <h1>Hey, There You Have Loged In To MIDAS IIITD.</h1>
         </div>
     </body>
